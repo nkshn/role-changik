ebca8e403ed654fe30c9f5c2db291e2e8ff21291
nkshn <vovanika23413@gmail.com>
2022-07-29 19:09:24 +0300

    added db config, migrations, seeds, and connecting to db

---
+++ config/database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const dbConfig = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+};
+
+module.exports = dbConfig[process.env.NODE_ENV];
