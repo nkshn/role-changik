5d9cb44b9be137766ec6ad20431ab47b6af30dd0
nkshn <vovanika23413@gmail.com>
2022-08-01 13:09:15 +0300

    added sing in route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,7 +14,7 @@ const singUp = async (req, res) => {
     const gettedUser = await findOneByEmail(email);
     if (gettedUser !== null) {
       return res.status(400).json({
-        data: 'User exist with such email address, try other!',
+        msg: 'User exist with such email address, try other!',
       });
     }
 
@@ -41,10 +41,35 @@ const singUp = async (req, res) => {
 
 const singIn = async (req, res) => {
   try {
-    const {} = req.body;
+    const { email, password } = req.body;
+
+    const user = await findOneByEmail(email);
+    if (user === null) {
+      return res.status(400).json({
+        msg: 'Invalid email or password, try again!',
+      });
+    }
+
+    const comparedPass = await bcrypt.compare(password, user.password);
+    if (!comparedPass) {
+      return res.status(400).json({
+        msg: 'Invalid email or password, try again!',
+      });
+    }
+
+    const token = createAccessToken(user.id, user.email);
+
+    return res.status(201).json({
+      msg: 'User successfully signed in!',
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    });
   } catch (err) {
     return res.status(500).json({
       message: 'Internal Error 500',
+      err: err.message,
     });
   }
 };
